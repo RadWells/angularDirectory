0e7342b3a53595c4578d6dde4773c776f39feb63
RadWells <scwells@icloud.com>
2015-03-11 19:16:33 -0400

    controller test

---
+++ angular/js/controllers.js
@@ -0,0 +1,12 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('MyController', function MyController($scope) {
+
+  $scope.author = {
+    'name' : 'John Ashbery',
+    'title' : 'Poet',
+    'company' : 'New York School'
+  }
+
+
+
+});
